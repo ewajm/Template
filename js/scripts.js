cee6f3c42ed65d69fa67e59bdc7cc77e44e80a06
Ewa Manek and Ryan Loos <ryan@>
2016-08-15 11:52:53 -0700

    Add template files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//<!-- to test if js file is connected  -->
+console.log('basic javascript is functioning')
+
+//<!-- to test if jquery is loaded  -->
+$(document).ready(function(){
+if (jQuery) {
+console.log('jQuery 3.1.0 is loaded');
+} else {
+console.log('jQuery is not loaded');
+    }
+});
